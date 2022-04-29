3cbe444bd8bcc967be6d7254a0148141ec788715
Trevon Mays <mays.trevon94@gmail.com>
2022-04-29 09:57:01 -0700

    commit tree a3599c1bc988815814b2e8ca755c5bf101ef3ecd

---
+++ index.js
@@ -0,0 +1,30 @@
+function shout(string) {
+    return string.toUpperCase();
+  }
+function whisper(string) {
+    return 'HELLO'.toLowerCase()
+}
+
+function logShout(string) {
+console.log(string.toUpperCase())
+}
+function logWhisper(string) {
+    console.log(string.toLowerCase())
+}
+function sayHiToHeadphonedRoommate(string) { 
+    if (string === string.toUpperCase()) {
+       return "YES INDEED!"
+    }
+    if (string === string.toLowerCase()) {
+     return "I can't hear you!"
+    }
+    if (`string ${"let's have dinner!"}`) {
+        return "I would love to!"
+    }
+    }
+    
+
+
+
+    
+
